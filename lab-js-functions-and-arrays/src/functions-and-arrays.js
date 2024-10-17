1a6ec5361b04428f8fedb6914ad36126e5e50369
Germán Álvarez <46670724+german-alvarez-dev@users.noreply.github.com>
2024-10-17 12:37:55 +0200

    Initial commit

---
+++ lab-js-functions-and-arrays/src/functions-and-arrays.js
@@ -0,0 +1,40 @@
+// Iteration 1 | Find the Maximum
+function maxOfTwoNumbers(value1, value2) {
+    if (value1 > value2) {
+        return value1
+    } else {
+        return value2
+    }
+}
+
+
+
+
+// Iteration 2 | Find the Longest Word
+const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
+
+function findLongestWord() { }
+
+
+
+
+// Iteration 3 | Sum Numbers
+const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+
+function sumNumbers() { }
+
+
+
+
+// Iteration 4 | Numbers Average
+const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
+
+function averageNumbers() { }
+
+
+
+
+// Iteration 5 | Find Elements
+const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
+
+function doesWordExist() { }
