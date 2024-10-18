9861c0fde24d0fa96db7d5029607e244c0446201
Germán Álvarez <46670724+german-alvarez-dev@users.noreply.github.com>
2024-10-18 10:19:30 +0200

    Initial commit

--- lab-js-functions-and-arrays/src/functions-and-arrays.js
+++ lab-js-functions-and-arrays/src/functions-and-arrays.js
@@ -13,7 +13,33 @@ function maxOfTwoNumbers(value1, value2) {
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() { }
+function findLongestWord(wordsArray) {
+
+    if (wordsArray.length === 0) {
+        return null
+    }
+
+    let longestWordFound = ''
+
+    // for (let i = 0; i < wordsArray.length; i++) {
+
+    //     const eachWord = wordsArray[i]
+
+    //     if (eachWord.length > longestWordFound.length) {
+    //         longestWordFound = eachWord
+    //     }
+    // }
+
+    wordsArray.forEach((eachWord) => {
+
+        if (eachWord.length > longestWordFound.length) {
+            longestWordFound = eachWord
+        }
+    })
+
+    return longestWordFound
+}
+
 
 
 
@@ -21,7 +47,22 @@ function findLongestWord() { }
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() { }
+function sumNumbers(numbersArray) {
+
+    let sum = 0
+
+    // for (let i = 0; i < numbersArray.length; i++) {
+
+    //     const eachNumber = numbersArray[i]
+
+    //     sum += eachNumber
+    // }
+
+
+    numbersArray.forEach((eachNumber) => sum += eachNumber)
+
+    return sum
+}
 
 
 
@@ -29,7 +70,18 @@ function sumNumbers() { }
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() { }
+function averageNumbers(numbersArray) {
+
+    if (numbersArray.length === 0) {
+        return 0
+    }
+
+    const total = sumNumbers(numbersArray)
+
+    const average = total / numbersArray.length
+
+    return average
+}
 
 
 
@@ -37,4 +89,30 @@ function averageNumbers() { }
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() { }
+function doesWordExist(wordsArray, wordToSearch) {
+
+    let exists = false
+
+    // for (let i = 0; i < wordsArray.length; i++) {
+
+    //     const eachWord = wordsArray[i]
+
+    //     if (eachWord === wordToSearch) {
+    //         exists = true
+    //     }
+    // }
+
+    wordsArray.forEach((eachWord) => {
+        if (eachWord === wordToSearch) {
+            exists = true
+        }
+    })
+
+    return exists
+}
+
+
+
+
+
+
